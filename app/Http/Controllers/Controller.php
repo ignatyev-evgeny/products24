ec3ac625cb35de9868189fa035dc32ea5b986034
Evgeny Ignatyev <ignatyev.ew@gmail.com>
2024-07-01 00:56:07 +0300

    Init контроллер

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
-    //
+    public static function successResponse(string $message = '', int $code = 200, array|bool $data = [])
+    {
+        $response = [
+            'success' => true,
+        ];
+
+        if (! empty($message)) {
+            $response['message'] = $message;
+        }
+
+        if (! empty($data)) {
+            $response['data'] = $data;
+        }
+
+        return response($response, $code);
+    }
+
+    public static function failResponse(array $data = [])
+    {
+        dd($data, \Illuminate\Support\Facades\Request::all());
+        return view('errorPage', $data);
+    }
 }
